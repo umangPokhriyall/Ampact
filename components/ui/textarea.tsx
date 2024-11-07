f2e656863d15e24e3042fe0daf38a8aa35299fb8
Umang Pokhriyal <umangpokhriyall@gmail.com>
2024-11-07 19:20:11 +0530

    now final

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,12 +1,8 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> { }
-
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
