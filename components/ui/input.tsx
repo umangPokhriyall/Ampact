5dec9d34b5c5a3a913c324292fa9ac977ae28a04
Umang Pokhriyal <umangpokhriyall@gmail.com>
2024-11-07 19:07:08 +0530

    fixed whatweoffer

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,11 +1,8 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> { }
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
